37d22823aed46d171c31939e611316c0545ca76e
wschoi <ws_choi@korea.ac.kr>
2020-10-10 15:43:18 +0900

    refactoring-tfc, tdc done

--- source_separation/models/model_definition.py
+++ source_separation/models/model_definition.py
@@ -1,11 +1,17 @@
 from warnings import warn
 
+from source_separation.models.tdc_net import TDC_NET_Framework
+from source_separation.models.tfc_net import TFC_NET_Framework
 from source_separation.models.tfc_tdf_net import TFC_TDF_NET_Framework
 
 def get_class_by_name(model_name):
 
     if model_name == 'tfc_tdf_net':
         return TFC_TDF_NET_Framework
+    elif model_name == 'tfc_net':
+        return TFC_NET_Framework
+    elif model_name == 'tdc_net':
+        return TDC_NET_Framework
     else:
         warn('please specify a model name: e.g. --model tfc_tdf_net')
         raise NotImplementedError
